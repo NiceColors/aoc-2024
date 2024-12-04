6edb4b8b6b0e1c787e46c1f1f669f6c36f6c6951
Victor Batista <victorbtst77@gmail.com>
2024-12-04 01:58:43 -0300

    Day 2: Red-Nosed Reports
    PART_ONE

---
+++ solutions/day_2.py
@@ -0,0 +1,60 @@
+text_path = "solutions/inputs/day_2.txt" # Path to the input file
+
+file = open(text_path, 'r')
+
+
+array = []
+
+for line in file:
+    array.append(list(map(int, line.strip().split())))
+
+safe_count = 0
+
+
+def verify_safety(arr):
+    
+    is_safe = True
+
+    difference_between_elements = 0
+
+    is_increasing = False
+    is_decreasing = False
+
+
+    for j in range(len(arr)):
+        
+        current = arr[j] 
+        prev = arr[abs(j-1)]
+
+        if current == prev:
+            is_safe = False
+            break
+        if j > 0:
+
+            difference_between_elements = abs(current - prev)
+            
+            if(not 1 <= difference_between_elements <= 3):
+                is_safe = False
+                break
+        if(j != len(arr) -1):
+            next = arr[j+1]
+            if (current > next):
+                is_decreasing = True
+            else:
+                is_increasing = True
+        if(is_decreasing and is_increasing):
+            is_safe = False
+            break
+    return is_safe
+
+# PART ONE
+
+for i in array:
+
+    is_safe = verify_safety(i)
+ 
+    if is_safe:
+        safe_count+=1
+
+print(safe_count)
+
