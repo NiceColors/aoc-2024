f2cc260b3f47c4f49ecbec335a7c7f5c421efdbe
Victor Batista <victorbtst77@gmail.com>
2024-12-14 21:19:43 -0300

    Day 5: Print Queue
    PART_TWO

--- solutions/day_5.py
+++ solutions/day_5.py
@@ -48,3 +48,34 @@ for i in numbers_of_each_update:
         arrays.append(i)
 
 print(sum_of_middles(arrays))
+
+
+#PART TWO
+
+invalid_arrays = []
+
+def is_update_line_incorrectly_ordered(line):
+    for number in line:
+        for i in page_ordering_rules:
+            if(number in i and i[1] in line and i[1] != number):
+                idx = line.index(number)
+                idx2 = line.index(i[1])
+
+                order_is_valid = idx < idx2
+                if(not order_is_valid):
+                    aux = line[idx]
+                    line[idx] = line[idx2]
+                    line[idx2] = aux
+
+                    new_line = line.copy()
+
+                    if(not is_update_line_incorrectly_ordered(new_line)):
+                        invalid_arrays.append(new_line)
+                    return True
+    return False
+
+
+for i in numbers_of_each_update:
+    is_update_line_incorrectly_ordered(i)
+
+print(sum_of_middles(invalid_arrays))
